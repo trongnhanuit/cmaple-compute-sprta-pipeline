071918c8c9b656d144ca54761f12aac2ba34a7d0
NhanLT <trongnhan.uit@gmail.com>
2024-08-30 13:44:59 +1000

    remove old results first

--- scripts/cmaple_compute_sprta.sh
+++ scripts/cmaple_compute_sprta.sh
@@ -25,4 +25,7 @@ done
                         
 echo "Moving the trees to ${TREE_DIR}"
 mkdir -p ${TREE_DIR}
+# remove old results
+rm -f ${TREE_DIR}/${CMAPLE_SPRTA_TREE_PREFIX}*treefile
+# copy new reaults
 mv ${ALN_DIR}/${CMAPLE_SPRTA_TREE_PREFIX}*treefile ${TREE_DIR}
