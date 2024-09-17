2d61b83c0b50f6b99deeadd42951ea6ea1a2a712
NhanLT <trongnhan.uit@gmail.com>
2024-09-17 10:58:42 +1000

    print sprta for less-informative sequences

--- scripts/cmaple_compute_sprta.sh
+++ scripts/cmaple_compute_sprta.sh
@@ -9,12 +9,18 @@ ML_TREE_PREFIX=$4 # The prefix of ML trees
 CMAPLE_SPRTA_TREE_PREFIX=$5 # The prefix of trees with SPRTA computed by CMAPLE
 MODEL=$6 # Substitution model
 BLENGTHS_FIXED=$7 # keep blengths fixed or not
+PRINT_LESS_INFO_SEQS=$8 # print SPRTA for less-informative sequences
 CMAPLE_PARAMS="-sprta -overwrite -search FAST" # CMAPLE params
+
 BL_FIXED_OPT=""
 if [ "${BLENGTHS_FIXED}" = true ]; then
   BL_FIXED_OPT=" -blfix"
 fi
 
+PRINT_LESS_INFO_SEQS_OPT=""
+if [ "${PRINT_LESS_INFO_SEQS}" = true ]; then
+  PRINT_LESS_INFO_SEQS_OPT=" -sprta-less-info-seqs"
+fi
 
 ### pre steps #####
 
@@ -26,8 +32,8 @@ for aln_path in "${ALN_DIR}"/*.maple; do
 	aln=$(basename "$aln_path")
     echo "Compute SPRTA for the tree ${ML_TREE_PREFIX}${aln}.treefile inferred from ${aln}"
     
-    echo "cd ${ALN_DIR} && ${CMAPLE_PATH} -aln ${aln} -t ${TREE_DIR}/${ML_TREE_PREFIX}${aln}.treefile -pre ${CMAPLE_SPRTA_TREE_PREFIX}${aln} ${CMAPLE_PARAMS} ${BL_FIXED_OPT}"
-    cd ${ALN_DIR} && ${CMAPLE_PATH} -aln ${aln} -t ${TREE_DIR}/${ML_TREE_PREFIX}${aln}.treefile -m ${MODEL} -pre ${CMAPLE_SPRTA_TREE_PREFIX}${aln} ${CMAPLE_PARAMS} ${BL_FIXED_OPT}
+    echo "cd ${ALN_DIR} && ${CMAPLE_PATH} -aln ${aln} -t ${TREE_DIR}/${ML_TREE_PREFIX}${aln}.treefile -pre ${CMAPLE_SPRTA_TREE_PREFIX}${aln} ${CMAPLE_PARAMS} ${BL_FIXED_OPT} ${PRINT_LESS_INFO_SEQS_OPT}"
+    cd ${ALN_DIR} && ${CMAPLE_PATH} -aln ${aln} -t ${TREE_DIR}/${ML_TREE_PREFIX}${aln}.treefile -m ${MODEL} -pre ${CMAPLE_SPRTA_TREE_PREFIX}${aln} ${CMAPLE_PARAMS} ${BL_FIXED_OPT} ${PRINT_LESS_INFO_SEQS_OPT}
 done
                         
 echo "Moving the trees to ${TREE_DIR}"
